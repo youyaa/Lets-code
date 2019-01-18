597595b1aa6fd1ae49ed2ebaacedd5203bbe4f8d
youyaa <listeningrain.cn@gmail.com>
2019-01-18 18:43:13 +0800

    更新部分变量的名称

--- 链表 LinkedList/20190118/CacheList.java
+++ 链表 LinkedList/20190118/CacheList.java
@@ -7,58 +7,58 @@ package LinkedList;
  */
 public class CacheList {
     //头节点
-    private SingleList head;
+    private SingleList HEAD;
     //尾节点
-    private SingleList tail;
+    private SingleList TAIL;
     //当前缓存的长度
-    private Integer currentListSize;
+    private Integer CURRENT_SIZE;
     //缓存的最大长度
     private Integer MAX_SIZE;
 
     //初始化
     public CacheList() {
         //初始化头尾节点为null
-        head = tail = new SingleList(null, null);
-        currentListSize = 0;
+        HEAD = TAIL = new SingleList(null, null);
+        CURRENT_SIZE = 0;
         MAX_SIZE = 10;
     }
 
     //初始化时设置缓存长度
     public CacheList(Integer MAX_SIZE) {
-        head = tail = new SingleList(null, null);
-        currentListSize = 0;
+        HEAD = TAIL = new SingleList(null, null);
+        CURRENT_SIZE = 0;
         this.MAX_SIZE = MAX_SIZE;
     }
 
     //放入缓存中
     public Boolean put(Integer key, Integer value) {
         //缓存已满，删除最后一个节点
-        if (currentListSize == MAX_SIZE) {
+        if (CURRENT_SIZE == MAX_SIZE) {
             System.out.println("缓存满咯，删除一个");
             //找到尾节点的前一个节点
-            SingleList next = head;
+            SingleList next = HEAD;
             while (null != next) {
-                if (next.next == tail) {
+                if (next.next == TAIL) {
                     //删除最后一个节点
-                    System.out.println("删除的节点key=" + tail.key + " value=" + tail.value);
+                    System.out.println("删除的节点key=" + TAIL.key + " value=" + TAIL.value);
                     next.next = null;
-                    tail = next;
+                    TAIL = next;
                     break;
                 }
                 next = next.next;
             }
-            currentListSize--;
+            CURRENT_SIZE--;
         }
 
         SingleList singleList = new SingleList(key, value);
-        SingleList next = head.next;
-        head.next = singleList;
+        SingleList next = HEAD.next;
+        HEAD.next = singleList;
         singleList.next = next;
-        if (head == tail) {
-            tail = singleList;
+        if (HEAD == TAIL) {
+            TAIL = singleList;
         }
 
-        currentListSize++;
+        CURRENT_SIZE++;
 
         return true;
     }
@@ -67,8 +67,8 @@ public class CacheList {
     public Integer get(Integer key) {
         boolean flag = false;
         Integer result = null;
-        SingleList next = head;
-        if (null != head) {
+        SingleList next = HEAD;
+        if (null != HEAD) {
             while (null != next) {
                 if (key.equals(next.key)) {
                     flag = true;
@@ -80,18 +80,18 @@ public class CacheList {
 
         if (true == flag) {
             //缓存中存在，先删除原位置的节点，再插入到第一个位置
-            SingleList next3 = head;
+            SingleList next3 = HEAD;
             while (null != next3) {
                 if (key.equals(next3.next.key)) {
                     //删除
                     SingleList old = next3.next;
                     next3.next = old.next;
-                    if (next3.next == tail) {
-                        tail = next3;
+                    if (next3.next == TAIL) {
+                        TAIL = next3;
                     }
                     //插入第一位
-                    SingleList next2 = head.next;
-                    head.next = old;
+                    SingleList next2 = HEAD.next;
+                    HEAD.next = old;
                     old.next = next2;
                     break;
                 }
@@ -104,10 +104,10 @@ public class CacheList {
 
     //打印链表
     public void printList() {
-        if (null == head) {
+        if (null == HEAD) {
             return;
         }
-        SingleList singleList = head;
+        SingleList singleList = HEAD;
         while (singleList != null) {
             System.out.print("key:" + singleList.key + " value:" + singleList.value);
             if (null != singleList.next) {
